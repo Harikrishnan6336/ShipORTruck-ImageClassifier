e735b26d0a0f60c6a8c0a759ff381d62657dbfbb
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-08-08 23:25:03 +0530

    .

---
+++ data/get_data.py
@@ -0,0 +1,35 @@
+"""
+The `labels.csv` and the contents of `cifar-10-dataset` directory can be obtained by downloading the CIFAR-10
+dataset from here - https://www.kaggle.com/c/cifar-10/data?select=test.7z
+"""
+
+import csv
+from skimage.io import imread
+from skimage.io import imsave
+
+fields = []
+rows = []
+required_labels = ['ship', 'truck']
+
+
+with open('labels.csv', 'r', newline='') as file:
+    csvreader = csv.reader(file)
+    # extracting field names through first row
+    fields = next(csvreader)
+  
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+
+for i in range(0,2):
+    # Procuring the images of a particular category in a directory of its own
+    for row in rows:
+        try:
+            if(row[1] == required_labels[i]):
+                img_path = 'cifar-10-dataset/' + (row[0]) + '.png'
+                img = imread(img_path)
+                imsave((required_labels[i]) + '/' + (row[0]) + '.png', img)
+        except Exception as e:
+            print(e)
+            exit()
+
---
+++ data/segregate_data.py
@@ -0,0 +1,19 @@
+import os
+from skimage.io import imread
+from skimage.io import imsave
+
+required_labels = ['ship', 'truck']
+
+for i in range(0,2):
+    j = 0
+    for file in os.listdir(required_labels[i]):
+        j = j + 1 
+        img_path =  (required_labels[i])  + '/' + (file)
+        img = imread(img_path)
+        # Separating the images for training and validation use and placing them in different directories. 
+        if(j < 4001):
+            imsave('dataset/train/'+ (required_labels[i])  + '/' + (file), img)
+        else:
+            imsave('dataset/valid/' +  (required_labels[i])  + '/' + (file), img)
+
+
