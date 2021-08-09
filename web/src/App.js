681ff7313858163ec79a1ee1c818df437a63ee60
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-08-09 17:18:31 +0530

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -11,10 +11,6 @@ function App() {
   const [imgData, setImgData] = useState("");
   const [result, setResult] = useState("");
 
-  const getBase64 = (str) => {
-    return str.split("base64,")[1];
-  };
-
   const convertBase64 = (file) => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -34,10 +30,12 @@ function App() {
     const file = e.target.files[0];
     const base64 = await convertBase64(file);
     setBaseImage(base64);
-    setImgData(getBase64(baseImage));
+    const baseString = base64.split("base64,")[1];
+    setImgData(baseString);
   };
 
   const identify = async (e) => {
+    console.log("Sending DATA ==  " + imgData);
     await axios
       .post(api_link, {
         image: imgData,
@@ -84,12 +82,24 @@ function App() {
         variant="primary"
         id="button-upload"
         onClick={() => {
-          setResult("");
-          identify();
+          if (baseImage === "") {
+            alert("No image uploaded");
+          } else {
+            setResult("");
+            identify();
+          }
         }}
       >
         <span className="button-text">Identify</span>
       </Button>
+
+      {/* <Button
+        onClick={() => {
+          console.log(imgData);
+        }}
+      >
+        debug
+      </Button> */}
     </div>
   );
 }
